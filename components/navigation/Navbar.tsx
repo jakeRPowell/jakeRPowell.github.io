cc82fe8e0d35e0a9799c00be4a20de44844547d2
jakeRPowell <ja.cobr@outlook.com>
2023-05-22 17:25:06 +0100

    start burger

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -2,29 +2,34 @@ import Page from '@/interfaces/page';
 import { getAllPages, getAllPosts } from '@/lib/api';
 import Link from 'next/link';
 import React from 'react';
+import BurgerMenu from './Burger';
 
 function Navbar() {
   return (
-    <ul className="align-centre flex flex-1 gap-5">
-      <li>
-        <Link href="/">
-          <h1 className="mb-0 text-2xl font-bold">Jacob Powell</h1>
-        </Link>
-      </li>
+    <>
+      <ul className="align-centre flex flex-1 gap-5">
+        <li>
+          <Link href="/">
+            <h1 className="mb-0 text-2xl font-bold">Jacob Powell</h1>
+          </Link>
+        </li>
 
-      <li className="flex items-center">
-        <Link href="/posts">Posts</Link>
-      </li>
-      <li className="flex items-center">
-        <Link href="/about">About</Link>
-      </li>
-      <li className="flex items-center">
-        <Link href="/contact">Contact</Link>
-      </li>
-      <li className="flex items-center">
-        <Link href="/404">404!</Link>
-      </li>
-    </ul>
+        <li className="flex hidden items-center md:block">
+          <Link href="/posts">Posts</Link>
+        </li>
+        <li className="flex hidden items-center md:block">
+          <Link href="/about">About</Link>
+        </li>
+        <li className="flex hidden items-center md:block">
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li className="flex hidden items-center md:block">
+          <Link href="/404">404!</Link>
+        </li>
+      </ul>
+
+      <BurgerMenu />
+    </>
   );
 }
 
